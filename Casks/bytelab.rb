3159fdc4ad4c6f26baeecb059953cb742438dba1
Playfair Display <playfairs@Playfairs-Mini.lan>
2025-08-22 17:20:46 -0500

    update sha

--- Casks/bytelab.rb
+++ Casks/bytelab.rb
@@ -1,6 +1,6 @@
 cask "bytelab" do
   version "1.1.0"
-  sha256 "<PASTE_SHA256>"
+  sha256 "d5c9d91a002873d44d568f9300aafe9ba54d4176da44ceaee2e331c51c83b291"
 
   url "https://github.com/ByteLabs-Studio/ByteLab/releases/download/v#{version}/bytelab-#{version}-arm64.dmg",
       verified: "github.com/ByteLabs-Studio/ByteLab/"
