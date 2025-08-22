1e28173399003780d015754373ad59f2fe9f2644
Playfair Display <playfairs@Playfairs-Mini.lan>
2025-08-22 17:17:23 -0500

    add cask bytelab

---
+++ Casks/bytelab.rb
@@ -0,0 +1,23 @@
+cask "bytelab" do
+  version "1.1.0"
+  sha256 "<PASTE_SHA256>"
+
+  url "https://github.com/ByteLabs-Studio/ByteLab/releases/download/v#{version}/bytelab-#{version}-arm64.dmg",
+      verified: "github.com/ByteLabs-Studio/ByteLab/"
+  name "ByteLab"
+  desc "Bytebeat Studio"
+  homepage "https://github.com/ByteLabs-Studio/ByteLab"
+
+  app "bytelab.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.bytelabs.bytelab.plist",
+    "~/Library/Application Support/ByteLab",
+    "~/Library/Saved Application State/com.bytelabs.bytelab.savedState",
+  ]
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+end
